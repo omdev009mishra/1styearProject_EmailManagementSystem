6bebcc623bf165ebd6f080de703db8281ebdf996
Pranjal Mishra <124626916+omdev009mishra@users.noreply.github.com>
2025-06-13 23:54:11 +0530

    Delete EmailManagementSystem_Debug directory

--- EmailManagementSystem_Debug/resources/email_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE emails (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sender VARCHAR(100),
-    recipient VARCHAR(100),
-    subject VARCHAR(150),
-    body TEXT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-)
--- EmailManagementSystem_Debug/src/Main.java
+++
@@ -1,9 +0,0 @@
-import ui.MainFrame;
-
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainFrame::new);
-    }
-}
